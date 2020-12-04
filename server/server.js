cef394376a0731d8d4293b048008d3c2b9e53bf6
gianmillare <gianmillaregsm@gmail.com>
2020-12-03 20:08:30 -0800

    started the app and created the server, package, and gitignore

---
+++ server/server.js
@@ -0,0 +1,13 @@
+// assign express to a variable and require it
+const express = require('express');
+
+// assign the express function to a variable
+const app = express();
+
+// connect the server to the public directory
+app.use(static.express('public'));
+
+// render the page to localhost3000
+app.listen(3000, function() {
+    console.log("App is running on localhost:3000!");
+});
