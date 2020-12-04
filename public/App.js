97da1809983a8be0a0fc9a4c2ed4be5683bc0b64
gianmillare <gianmillaregsm@gmail.com>
2020-12-03 20:44:25 -0800

    completed the placeholder classes for the Issue Tracker app

--- public/App.js
+++ public/App.js
@@ -1,19 +1,45 @@
-class HelloWorld extends React.Component {
+// class HelloWorld extends React.Component {
+//     render() {
+//         const continents = ['Africa', 'Asia', 'America', 'Australia', 'Europe'];
+//         const helloContinents = [];
+//         for (var i = 0; i < continents.length; i++) {
+//             helloContinents.push(`Hello ${continents[i]}!`);
+//         }
+//         const message = helloContinents.join(' ');
+//         return (
+//             <div title='Outer div'>
+//                 <h1>{message}</h1>
+//             </div>
+//         );
+//     }
+// }
+class IssueFilter extends React.Component {
   render() {
-    const continents = ['Africa', 'Asia', 'America', 'Australia', 'Europe'];
-    const helloContinents = [];
-
-    for (var i = 0; i < continents.length; i++) {
-      helloContinents.push(`Hello ${continents[i]}!`);
-    }
-
-    const message = helloContinents.join(' ');
-    return /*#__PURE__*/React.createElement("div", {
-      title: "Outer div"
-    }, /*#__PURE__*/React.createElement("h1", null, message));
+    return /*#__PURE__*/React.createElement("div", null, "This area will hold all the issue filters!");
   }
 
 }
 
-const element = /*#__PURE__*/React.createElement(HelloWorld, null);
+class IssueTable extends React.Component {
+  render() {
+    return /*#__PURE__*/React.createElement("div", null, "This area will hold a table of issues!");
+  }
+
+}
+
+class IssueAdd extends React.Component {
+  render() {
+    return /*#__PURE__*/React.createElement("div", null, "This area will hold a form to add an issue!");
+  }
+
+}
+
+class IssueList extends React.Component {
+  render() {
+    return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Issue Tracker"), /*#__PURE__*/React.createElement(IssueFilter, null), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(IssueTable, null), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(IssueAdd, null));
+  }
+
+}
+
+const element = /*#__PURE__*/React.createElement(IssueList, null);
 ReactDOM.render(element, document.getElementById('contents'));
--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,66 @@
-class HelloWorld extends React.Component {
+// class HelloWorld extends React.Component {
+//     render() {
+//         const continents = ['Africa', 'Asia', 'America', 'Australia', 'Europe'];
+//         const helloContinents = [];
+
+//         for (var i = 0; i < continents.length; i++) {
+//             helloContinents.push(`Hello ${continents[i]}!`);
+//         }
+
+//         const message = helloContinents.join(' ');
+
+//         return (
+//             <div title='Outer div'>
+//                 <h1>{message}</h1>
+//             </div>
+//         );
+//     }
+// }
+
+class IssueFilter extends React.Component {
     render() {
-        const continents = ['Africa', 'Asia', 'America', 'Australia', 'Europe'];
-        const helloContinents = [];
-
-        for (var i = 0; i < continents.length; i++) {
-            helloContinents.push(`Hello ${continents[i]}!`);
-        }
-
-        const message = helloContinents.join(' ');
-
         return (
-            <div title='Outer div'>
-                <h1>{message}</h1>
-            </div>
+            <div>This area will hold all the issue filters!</div>
         );
     }
 }
 
-const element = <HelloWorld />;
+class IssueTable extends React.Component {
+    render() {
+        return (
+            <div>This area will hold a table of issues!</div>
+        );
+    }
+}
+
+class IssueAdd extends React.Component {
+    render() {
+        return (
+            <div>This area will hold a form to add an issue!</div>
+        );
+    }
+}
+
+class IssueList extends React.Component{
+    render() {
+        return (
+            <React.Fragment>
+                {/* Insert the title of the App */}
+                <h1>Issue Tracker</h1>
+
+                {/* Insert the IssueFilter class */}
+                <IssueFilter />
+                <hr/>
+
+                {/* Insert the IssueTable class */}
+                <IssueTable />
+                <hr />
+
+                {/* Insert the IssueAdd class */}
+                <IssueAdd />
+            </React.Fragment>
+        )
+    }
+}
+const element = <IssueList />;
 ReactDOM.render(element, document.getElementById('contents'));
