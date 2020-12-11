8d191c8212149a0f60daebbd38713fcf9d410dd3
gianmillare <gianmillaregsm@gmail.com>
2020-12-10 17:45:01 -0800

    completed the IssueTable function

--- src/App.jsx
+++ src/App.jsx
@@ -35,5 +35,24 @@ function IssueRow(props) {
 }
 
 function IssueTable(props) {
-    
+    const issueRows = props.issues.map(issue => <IssueRow key={issue.id} issue={issue}/>);
+    return (
+        <table className="bordered-table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Status</th>
+                    <th>Owner</th>
+                    <th>Created</th>
+                    <th>Effort</th>
+                    <th>Due Date</th>
+                    <th>Title</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                {issueRows}
+            </tbody>
+        </table>
+    );
 }
