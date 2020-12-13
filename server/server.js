fcd4c3e16395c465a8a50b299fb93c0f44abbbb4
gianmillare <gianmillaregsm@gmail.com>
2020-12-13 11:09:23 -0800

    completed adding graphQL and apollo server to the server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,49 @@
+// set express to require express
 const express = require('express');
+
+// Create an apollo server object
+const { ApolloServer } = require('apollo-server-express');
+
+// the variable that we want to display: an about message
+let aboutMessage = "Issue Tracker API v1.0";
+
+// define the schema using string formatting
+const typeDefs = `
+    type Query {
+        about: String!
+    }
+    type Mutation {
+        setAboutMessage(message: String!): String
+    }
+`;
+
+// create functions following the schema that will be used when accessing the schema
+const resolvers = {
+    Query: {
+        about: () => aboutMessage,
+    },
+    Mutation: {
+        setAboutMessage,
+    },
+};
+
+// define the function used in the resolvers above
+function setAboutMessage(_, {message}) {
+    return aboutMessage = message;
+}
+
+// initiate an apollo server that takes in the typedefs and the resolvers above
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
 
 const app = express();
 
 app.use(express.static('public'));
+
+// install the apollo server as a middleware in Express
+server.applyMiddleware({ app, path: '/graphql' });
 
 app.listen(3000, function() {
     console.log("App is running on localhost:3000!");
