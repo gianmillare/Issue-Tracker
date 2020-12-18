e2b85c233350d40a28773965383616f0eef2d661
gianmillare <gianmillaregsm@gmail.com>
2020-12-17 20:42:01 -0800

    replaced the original query in createIssue with a query variable

--- src/App.jsx
+++ src/App.jsx
@@ -108,12 +108,8 @@ class DisplayIssue extends React.Component {
     }
 
     async createIssue(issue) {
-        const query = `mutation {
-            issueAdd(issue: {
-                title: "${issue.title}",
-                owner: "${issue.owner}",
-                due: "${issue.due.toISOString()}",
-            }) {
+        const query = `mutation issueAdd($issue: IssueInputs!) {
+            issueAdd(issue: $issue) {
                 id
             }
         }`;
@@ -121,7 +117,7 @@ class DisplayIssue extends React.Component {
         const response = await fetch('/graphql', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ query })
+            body: JSON.stringify({ query, variables: { issue } })
         });
         this.loadData();
     }
