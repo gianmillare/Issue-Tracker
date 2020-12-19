f254fd95c05afab40d07ed75a75c421b9c6ad0e4
gianmillare <gianmillaregsm@gmail.com>
2020-12-18 19:49:41 -0800

    completed the error catching feature in the graphQLFetch function in app.jsx

--- server/server.js
+++ server/server.js
@@ -104,6 +104,21 @@ async function graphQLFetch(query, variables = {}) {
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ query, variables })
         });
+        const body = await response.text();
+        const result = JSON.parse(body, jsonDataReviver);
+
+        // if errors exist in the results, display the first error
+        if (result.errors)  {
+            const error = result.errors[0];
+            // if the errors return bas user inputs, return the error and separate them by line
+            if (error.extensions.code == 'BAD_USER_INPUT') {
+                const details = error.extensions.exception.errors.join('\n ');
+                alert(`${error.message}:\n ${details}`);
+            } else {
+                alert(`${error.extensions.code}: ${error.message}`);
+            }
+        }
+        return result.data;
     }
 }
 
